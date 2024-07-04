ae673ee234161084c667e096bb50b36cb8913697
hajimshenga <hajismshenga20@gmail.com>
2024-07-04 13:53:10 +0300

    first commit

--- src/main/java/com/tunguu/student/controller/EmployeeController.java
+++ src/main/java/com/tunguu/student/controller/EmployeeController.java
@@ -5,7 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +29,40 @@ public class EmployeeController {
         List<Employee> emp = employeeServices.getAllEmployees();
         return new ResponseEntity<>(emp,HttpStatus.OK);
     }
-    
+
+    @PostMapping("/add")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee emp)
+    {
+        Employee savedEmployee = employeeServices.addEmployee(emp);
+        return new ResponseEntity<>(savedEmployee,HttpStatus.CREATED);
+    }
+
+    @GetMapping("/employeeById/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long empId)
+    {
+        Employee emp = employeeServices.getEmployeeById(empId);
+        return new ResponseEntity<>(emp,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long empId)
+    {
+        employeeServices.deleteEmployee(empId);
+        return new ResponseEntity<String>("Employee deleted sucessfully",HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable("id") Long empId, @RequestBody Employee emp)
+    {
+        emp.setId(empId);
+        Employee empUpdate = employeeServices.updateEmployee(emp);
+        return new ResponseEntity<>(empUpdate,HttpStatus.CREATED);
+    }
+
+
+
+
+
+   
     
 }
--- src/main/java/com/tunguu/student/services/EmployeeServices.java
+++ src/main/java/com/tunguu/student/services/EmployeeServices.java
@@ -1,6 +1,7 @@
 package com.tunguu.student.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,8 +14,31 @@ public class EmployeeServices {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+    
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    public Employee addEmployee(Employee emp) {
+        return employeeRepository.save(emp);
+    }
+
+    public Employee getEmployeeById(Long empId) {
+        Optional<Employee> optionalEmployee = employeeRepository.findById(empId);
+        return optionalEmployee.get();
+    }
+
+    public void deleteEmployee(Long empId) {
+        employeeRepository.deleteById(empId);
+    }
+
+    public Employee updateEmployee(Employee emp) {
+        Employee existingEmp = employeeRepository.findById(emp.getId()).get();
+        existingEmp.setFirstName(emp.getFirstName());
+        existingEmp.setLastName(emp.getLastName());
+        existingEmp.setEmail(emp.getEmail());
+        Employee updateEmployee = employeeRepository.save(existingEmp);
+        return updateEmployee;
     }
     
 }
